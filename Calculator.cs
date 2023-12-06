1aadffe014e716c554265063a2bf1ffd8eae2ef5
Dozixary Lawrence <Dozixary57@hotmail.com>
2023-12-06 07:23:15 +0300

    Comments have been added to most of the project code.

--- Calculator.cs
+++ Calculator.cs
@@ -5,42 +5,56 @@ using System.Text;
 using System.Threading.Tasks;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
+// Delegate representing a mathematical function that takes a double parameter and returns a double
 public delegate double Function(double x);
 
 namespace NUnitTDDTests
 {
+    // Calculator class with various mathematical operations
     internal class Calculator
     {
+        // Method to perform division operation
         public double Div(double a, double b)
         {
+            // Check if the divisor is close to zero to avoid division by nearly zero
             if (Math.Abs(b) < double.Epsilon)
             {
+                // If divisor is close to zero, return NaN (Not a Number)
                 return double.NaN;
             }
+            // Perform the division and return the result
             return a / b;
         }
 
+        // Method to perform exponentiation operation
         public double Pow(double a, double b)
         {
+            // Use Math.Pow to calculate a raised to the power of b
             return Math.Pow(a, b);
         }
 
+        // Method to calculate the factorial of a given integer
         public int Fac(int n)
         {
+            // Base case: factorial of 0 is 1
             if (n == 0)
             {
                 return 1;
             }
+            // Recursive case: calculate factorial using recursion
             else
             {
                 return n * Fac(n - 1);
             }
         }
 
+        // Constant representing a small value used in numerical differentiation
         private const double h = 0.0001;
 
+        // Static method to calculate the numerical derivative of a function at a given point
         public static double Der(Function f, double x)
         {
+            // Numerical differentiation using the central difference formula
             return (f(x + h) - f(x - h)) / (2 * h);
         }
     }
--- CalculatorTests.cs
+++ CalculatorTests.cs
@@ -8,16 +8,20 @@ using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace NUnitTDDTests
 {
+    // Test class for the Calculator class using NUnit framework
     internal class CalculatorTests
     {
+        // Instance of the Calculator class to be tested
         private Calculator calculator;
 
+        // Setup method executed before each test to initialize the Calculator instance
         [SetUp]
         public void Setup()
         {
             calculator = new Calculator();
         }
 
+        // Test method for the Div operation with multiple test cases
         [TestCase(25, 5, 5)]
         [TestCase(5, 2.5, 2)]
         [TestCase(-1, 5, -0.2)]
@@ -25,10 +29,12 @@ namespace NUnitTDDTests
         [TestCase(5, 0, double.NaN)]
         public void TestDiv(double a, double b, double expected)
         {
+            // Call the Div method and assert the result
             var result = calculator.Div(a, b);
             Assert.AreEqual(expected, result);
         }
 
+        // Test method for the Pow operation with multiple test cases
         [TestCase(4, 3, 64)]
         [TestCase(3, 0, 1)]
         [TestCase(5, -1, 0.2)]
@@ -36,10 +42,12 @@ namespace NUnitTDDTests
         [TestCase(25, 0.5, 5)]
         public void TestPow(double a, double b, double expected)
         {
+            // Call the Pow method and assert the result
             var result = calculator.Pow(a, b);
             Assert.AreEqual(expected, result);
         }
 
+        // Test method for the Fac operation with multiple test cases
         [TestCase(1, 1)]
         [TestCase(3, 6)]
         [TestCase(5, 120)]
@@ -47,22 +55,29 @@ namespace NUnitTDDTests
         [TestCase(10, 3628800)]
         public void TestFac(int n, int expected)
         {
+            // Call the Fac method and assert the result
             var result = calculator.Fac(n);
             Assert.AreEqual(expected, result);
         }
 
+        // Test method for the Der operation with a custom function and expected derivatives
         [Test]
         public void TestDer()
         {
+            // Define a sample function for testing the derivative
             Function myFunction = x => 2 * x * x + 3 * x + 1;
+
+            // Define expected derivative values at specific points
             double expectedDerivativeAt1 = 7.0;
             double expectedDerivativeAt2 = 11.0;
             double expectedDerivativeAt3 = 15.0;
 
+            // Calculate actual derivatives using the Calculator's Der method
             double actualDerivativeAt1 = Calculator.Der(myFunction, 1.0);
             double actualDerivativeAt2 = Calculator.Der(myFunction, 2.0);
             double actualDerivativeAt3 = Calculator.Der(myFunction, 3.0);
 
+            // Assert the calculated derivatives against the expected values with a tolerance of 0.001
             Assert.AreEqual(expectedDerivativeAt1, actualDerivativeAt1, 0.001);
             Assert.AreEqual(expectedDerivativeAt2, actualDerivativeAt2, 0.001);
             Assert.AreEqual(expectedDerivativeAt3, actualDerivativeAt3, 0.001);
